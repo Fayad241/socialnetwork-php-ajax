95155d723c60b39bf35d2c410f6c21f891b0d722
Fayad ROUFAI <fayadroufai241@gmail.com>
2025-12-05 04:18:57 +0100

    Responsive page de creation de posts

--- inclusions/components/mobileNavigation.php
+++ inclusions/components/mobileNavigation.php
@@ -6,8 +6,8 @@
     <title>Document</title>
 </head>
 <body>
-    <nav class="flex lg:hidden bg-white border-t border-gray-200 p-2 sticky bottom-0 z-50">
-        <div class="flex justify-between gap-8 items-center">
+    <nav class="flex lg:hidden bg-white border-t border-gray-200 p-2 sticky bottom-0 z-50 w-full">
+        <div class="flex justify-between gap-8 items-center w-full">
             <a href="/socialnetwork/home.php" class="flex flex-col items-center gap-1 p-2 text-blue-600">
                 <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
                     <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
